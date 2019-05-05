12a7b732917c2535b8f3d66c40b2fa3d4e21edc7
Dylan Feltus <contact@dylanfeltus.com>
2019-05-05 19:02:29 -0400

    Active Admin

--- lib/generators/autopilot/admin_generator.rb
+++ lib/generators/autopilot/admin_generator.rb
@@ -5,6 +5,19 @@ module Autopilot
 
       def do_magic
         puts "Set up active admin"
+
+        gem "activeadmin"
+        Bundler.with_clean_env do
+          run "bundle install"
+        end
+        generate "active_admin:install"
+        run "db:migrate"
+        run "db:seed"
+
+        puts "Admin account generated:"
+        puts "Email: admin@example.com"
+        puts "Password: password"
+        puts "Access your admin account at /admin"
       end
     end
   end
