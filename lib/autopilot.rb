22e0b6fd97e3caf3535711ba47676926715a5150
Dylan Feltus <contact@dylanfeltus.com>
2019-05-05 19:09:41 -0400

    Cleanup

--- lib/autopilot.rb
+++ lib/autopilot.rb
@@ -1,16 +1,4 @@
 require "autopilot/railtie"
 
 module Autopilot
-  class << self
-    attr_accessor :configuration
-  end
-
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
-  end
-
-  class Configuration
-    attr_accessor :multiple_users, :super_admin, :home_page
-  end
 end
